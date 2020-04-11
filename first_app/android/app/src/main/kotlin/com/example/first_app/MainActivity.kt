32b8035f3d19dfa28ab8ebcaf2fdfc60b64fe67a
namkeke <phuongnamhn2000@gmail.com>
2020-04-11 14:44:06 +0700

    2nd

--- first_app/android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
+++ first_app/android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.helloworldflutterapp
+package com.example.first_app
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
---
+++ first_app/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomListWordsState createState() => RandomListWordsState();
+}
+
+class RandomListWordsState extends State<RandomWords> {
+  final _words = <WordPair>[];
+  final _checkedWords = new Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("List of Eng words"),
+      ),
+      body: ListView.builder(itemBuilder: (context, index) {
+        if (index >= _words.length) {
+          _words.addAll(generateWordPairs().take(5));
+        }
+        return _buildRow(_words[index], index);
+      }),
+    );
+  }
+
+  Widget _buildRow(WordPair word, int index) {
+    final color = index % 2 == 0 ? Colors.red : Colors.blue;
+    final isChecked = _checkedWords.contains(word);
+    return ListTile(
+      //leading = left ,trailing = right
+      trailing: Icon(isChecked ? Icons.favorite : Icons.favorite_border,color: Colors.red,),
+      leading: Icon(isChecked ? Icons.check_box : Icons.check_box_outline_blank,color: color,),
+      title: Text(
+        word.asPascalCase,
+        style: TextStyle(fontSize: 20.0, color: color),
+      ),
+      onTap: (){
+        setState(() {
+          //anonymous function
+          if(isChecked)
+            _checkedWords.remove(word);
+          else
+            _checkedWords.add(word);
+        });
+      },
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: RandomWords(),
+    );
+  }
+}
